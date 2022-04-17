f9388dac19919383a899eab8e9e72dacd4ed3360
Francesco Belacca <francesco.belacca@agic.cloud>
2022-04-17 19:09:52 +0200

    started implementation following along

---
+++ src/WisdomPetMedicine.Hospital.Infrastructure/PatientAggrefateStore.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using WisdomPetMedicine.Hospital.Domain.Entities;
+using WisdomPetMedicine.Hospital.Domain.Repositories;
+using WisdomPetMedicine.Hospital.Domain.ValueObjects;
+
+namespace WisdomPetMedicine.Hospital.Infrastructure
+{
+    public class PatientAggrefateStore : IPatientAggregateStore
+    {
+        private readonly CosmosClient _client;
+        private readonly Container _container;
+
+        public PatientAggrefateStore(IConfiguration configuration)
+        {
+            var cs = configuration["CosmosDb:ConnectionString"];
+            var dbId = configuration["CosmosDb:DatabaseId"];
+            var containerId = configuration["CosmosDb:ContainerId"];
+
+            _client = new CosmosClient(cs, new CosmosClientOptions()
+            {
+                SerializerOptions = new CosmosSerializationOptions()
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            });
+
+            _container = _client.GetContainer(dbId, containerId);
+        }
+        public Task<Patient> LoadAsync(PatientId patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
